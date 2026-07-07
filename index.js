797216b5d2fd7a48ed6c7a68c1c8778abb40e276
Dolapo24-byte <dolapomakinde50@gmail.com>
2026-07-07 13:24:04 -0700

    done
    doen

---
+++ index.js
@@ -0,0 +1,31 @@
+async function fetchdata(){
+
+    try {
+        
+        const response = await fetch("https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json");
+
+        const data = await response.json();
+        console.log(data);
+
+        const result = document.getElementById("results");
+
+        result.innerHTML = data.map((item) => {
+        return `
+            <div class="card"> 
+                <h3>name: ${item.name} </h3>
+                <p>price: ${item.price} </p>
+                <p>images: ${item.image}</p>
+                <p>Type: ${item.type}</p>
+            </div>
+        
+        `
+        }).join("")
+
+
+    } catch (error) {
+        console.log("error");
+    }
+
+}
+
+fetchdata();
